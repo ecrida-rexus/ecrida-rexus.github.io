7ce4814ad63c98dad476243fe92ee24a7df466f8
Mihai Predoiu <predoiumihai@gmail.com>
2020-03-13 02:08:21 +0200

    Display Introduction image using react-image-gallery package

--- src/components/Introduction/index.tsx
+++ src/components/Introduction/index.tsx
@@ -1,8 +1,11 @@
 import React from 'react'
 
+import ImageGallery from 'react-image-gallery'
+
 import RexusBexus from './assets/RexusBexus.png'
 import Logo from './assets/Logo.svg'
-import TeamImage from './assets/TeamImage.jpg'
+import Noordwijk from './assets/Noordwijk.jpg'
+import Kiruna from './assets/Kiruna.jpg'
 import Decoration from './assets/Decoration.svg'
 
 import './index.scss'
@@ -12,6 +15,17 @@ export interface IIntroductionProps {
 
 export interface IIntroductionState {
 }
+
+const images = [
+  {
+    original: Kiruna,
+    description: 'Kiruna, Sweden'
+  },
+  {
+    original: Noordwijk,
+    description: 'Noordwijk, Netherlands, ESTEC Centre'
+  },
+];
 
 export default class Introduction extends React.Component<IIntroductionProps, IIntroductionState> {
   state: IIntroductionState = {
@@ -30,7 +44,8 @@ export default class Introduction extends React.Component<IIntroductionProps, II
             </div>
             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
           </div>
-          <img src={TeamImage} className='Image' alt='decoration' />
+          {/* <img src={TeamImage} className='Image' alt='decoration' /> */}
+          <ImageGallery items={images} showThumbnails={false} showFullscreenButton={false} showPlayButton={false} showBullets />
         </div>
         <img src={Decoration} className='Decoration' alt='decoration' />
       </div >
