f87b42ce53f289a6bc71dc583cc40f8243c84d62
Mihai Predoiu <predoiumihai@gmail.com>
2020-04-06 04:17:53 +0300

    Update main description text

--- src/components/Introduction/index.tsx
+++ src/components/Introduction/index.tsx
@@ -51,23 +51,23 @@ export default class Introduction extends React.Component<IIntroductionProps, II
               </a>
             </div>
             <p>
-              ECRIDA is a Romanian multidisciplinary team of 10 students from 2
-              universities which participates in the REXUS CYCLE 13 programme.
-              We propose the study of the 3D printing phenomenon using UV resin
-              polymerization in a low gravity environment.
+              ECRIDA is multidisciplinary team of 10 students from
+              Romania participating in the REXUS/BEXUS programme. Our
+              experiment aims to print 3D samples in a low gravity
+              environment using the UV photopolymerization phenomenon.
             </p>
             <p>
-              REXUS is a campaign organized by a bilateral Agency
-              Agreement between the German Aerospace Center (DLR)
-              and the Swedish National Space Agency (SNSA) in
-              cooperation with European Space Agency ESA. It gives
-              European students the opportunity to carry out
-              scientific experiments in low gravity conditions
-              on-board a sounding rocket.
+              REXUS/BEXUS is a campaign organized by a bilateral Agency
+              Agreement between the German Aerospace Center (DLR) and the
+              Swedish National Space Agency (SNSA) in cooperation with
+              European Space Agency (ESA). It gives European students the
+              opportunity to carry out scientific experiments on-board
+              sounding rockets and high-altitude balloons.
             </p>
             <p>
-              Our Experiment will be launched inside the RX29 rocket in 2021 from the Polar Circle.
-</p>
+              Our experiment will be launched on-board the RX29 Rocket
+              from the Polar Circle, Sweden in March 2021.
+            </p>
           </div>
           <ImageGallery items={images} showThumbnails={false} showFullscreenButton={false} showPlayButton={false} showBullets />
         </div>
