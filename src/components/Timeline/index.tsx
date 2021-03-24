ef9a78f726ad5dc42f232b9e6fd7aef11b051811
Mihai Predoiu <predoiumihai@gmail.com>
2021-03-24 10:08:21 +0200

    Update Facebook posts

--- src/components/Timeline/index.tsx
+++ src/components/Timeline/index.tsx
@@ -8,6 +8,48 @@ export default function Timeline() {
       <div className='Row'>
         <div
           className='fb-post'
+          data-href="https://www.facebook.com/projectecrida/posts/503271701086323"
+          data-show-text='true'
+          data-width='350'
+        >
+          <blockquote
+            cite="https://www.facebook.com/projectecrida/posts/503271701086323"
+            className='fb-xfbml-parse-ignore'
+          >
+            <p>[RO] Astăzi îl serbăm pe dragul nostru coleg si prieten Paraschiv Paul. Sperăm că ai avut o zi frumoasă și îți urăm...</p>
+              Posted by
+              <a href='https://www.facebook.com/projectecrida/'>
+              REXUS - Project Ecrida
+              </a>
+              on
+              <a href="https://www.facebook.com/projectecrida/posts/503271701086323">
+              Sunday, March 21, 2021
+              </a>
+          </blockquote>
+        </div>
+        <div
+          className='fb-post'
+          data-href="https://www.facebook.com/projectecrida/posts/492705845476242"
+          data-show-text='true'
+          data-width='350'
+        >
+          <blockquote
+            cite="https://www.facebook.com/projectecrida/posts/492705845476242"
+            className='fb-xfbml-parse-ignore'
+          >
+            <p>[RO] Suntem foarte bucuroși să vă anunțăm că proiectului nostru i s-a alăturat un nou sponsor. 2Space este o organizație formată din studenți politehniști ...</p>
+              Posted by
+              <a href='https://www.facebook.com/projectecrida/'>
+              REXUS - Project Ecrida
+              </a>
+              on
+              <a href="https://www.facebook.com/projectecrida/posts/492705845476242">
+              Friday, March 5, 2021
+              </a>
+          </blockquote>
+        </div>
+        <div
+          className='fb-post'
           data-href="https://www.facebook.com/projectecrida/posts/485208182892675"
           data-show-text='true'
           data-width='350'
@@ -24,48 +66,6 @@ export default function Timeline() {
               on
               <a href="https://www.facebook.com/projectecrida/posts/485208182892675">
               Sunday, February 21, 2021
-              </a>
-          </blockquote>
-        </div>
-        <div
-          className='fb-post'
-          data-href="https://www.facebook.com/projectecrida/posts/479300940150066"
-          data-show-text='true'
-          data-width='350'
-        >
-          <blockquote
-            cite="https://www.facebook.com/projectecrida/posts/479300940150066"
-            className='fb-xfbml-parse-ignore'
-          >
-            <p>Let the integration begin!</p>
-              Posted by
-              <a href='https://www.facebook.com/projectecrida/'>
-              REXUS - Project Ecrida
-              </a>
-              on
-              <a href="https://www.facebook.com/projectecrida/posts/479300940150066">
-              Friday, February 12, 2021
-              </a>
-          </blockquote>
-        </div>
-        <div
-          className='fb-post'
-          data-href="https://www.facebook.com/projectecrida/posts/478642690215891"
-          data-show-text='true'
-          data-width='350'
-        >
-          <blockquote
-            cite="https://www.facebook.com/projectecrida/posts/478642690215891"
-            className='fb-xfbml-parse-ignore'
-          >
-            <p>[EN]  *drum roll, please* <span role='img' aria-label=''>🥁</span> HAPPY BIRTHDAY, Cosmin !! <span role='img' aria-label=''>🥳🥳</span> We would like to wish an awesome birthday to our musician...</p>
-              Posted by
-              <a href='https://www.facebook.com/projectecrida/'>
-              REXUS - Project Ecrida
-              </a>
-              on
-              <a href="https://www.facebook.com/projectecrida/posts/478642690215891">
-              Thursday, February 11, 2021
               </a>
           </blockquote>
         </div>
